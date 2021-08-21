7083cb298fbf75928f1f75ff40b213b511087225
Tim <kindert1@tcnj.edu>
2021-08-21 02:02:49 -0400

    Tried to factor sieve code out into a procedure but it works differently for some reason

--- Irvine_Chapter_9/Irvine_Chapter_9/Problem_9_7.asm
+++ Irvine_Chapter_9/Irvine_Chapter_9/Problem_9_7.asm
@@ -15,6 +15,57 @@ ENDIF
 .code
 
 
+sieve_of_Eratosthenes PROC
+	sieve_pointer EQU [ebp + 8]
+	N EQU [ebp + 12]
+
+	push ebp
+	mov ebp, esp
+
+	mov eax, 2
+	inc BYTE PTR sieve_pointer[0]			; 0 is not prime
+	inc BYTE PTR sieve_pointer[1]			; 1 Is not prime
+next_prime:
+	mov esi, sieve_pointer
+	add esi, eax					; Start this loop at the index of the next prime in the sequence.
+
+inner_loop:
+	; "Cross off" each multiple of the current prime
+	add esi, eax
+	mov ebx, sieve_pointer
+	add ebx, N
+	cmp esi, ebx
+	jge break
+	mov BYTE PTR [esi], 1
+
+	jmp inner_loop
+
+break:
+	; Starting from 1 after the last prime, search for the next zero entry (i.e. the next prime)
+	mov edi, sieve_pointer
+	add edi, eax
+	add edi, 1
+	mov al, 0
+
+	mov ecx, N						; Make sure scasb does not terminate early because ecx reaches 0
+	cld
+	repne scasb
+	dec edi
+
+	; Find the offset of the pointer to the zero that was found by scasb. This is exactly the next prime number, which will be used in the next iteration.
+	mov eax, edi
+	sub eax, sieve_pointer
+
+	cmp eax, sqrt_65000				; The well-known optimization of the sieve lets us quit once we reach sqrt(N), and in this case, N = 65,000
+	jb next_prime
+
+	mov esp, ebp
+	pop ebp
+
+	ret 8
+sieve_of_Eratosthenes ENDP
+
+
 p97 PROC
 	; The Sieve of Eratosthenes provides a quick way to find all prime numbers within a given range.
 	; The algorimth involves creating an array of bytes in which positions are "marked" by inserting 1s in the following manner:
@@ -27,39 +78,9 @@ p97 PROC
 	; For this program, create a 65,000 element array and display all primes between 2 and 65,000.
 	; Declare the array in an uninitialized data segment and use STOSB to fill it with zeros.
 	
-	mov eax, 2
-	inc BYTE PTR sieve[0]			; 0 is not prime
-	inc BYTE PTR sieve[1]			; 1 Is not prime
-next_prime:
-	lea esi, [OFFSET sieve + eax]	; Start this loop 
-
-inner_loop:
-	add esi, eax
-	lea ebx, [sieve + LENGTHOF sieve]
-	cmp esi, OFFSET sentinel
-	jge break
-	mov BYTE PTR [esi], 1
-
-	jmp inner_loop
-
-break:
-	; Starting from 1 after the last prime, search for the next zero entry (i.e. the next prime)
-	lea edi, [OFFSET sieve + eax + 1]		
-	mov al, 0
-
-	mov ecx, LENGTHOF sieve			; Make sure scasb does not terminate early because ecx reaches 0
-	cld
-	repne scasb
-	dec edi
-
-	; Find the offset of the pointer to the zero that was found by scasb. This is exactly the next prime number, which will be used in the next iteration.
-	mov eax, edi
-	sub eax, OFFSET sieve
-
-	cmp eax, sqrt_65000				; The well-known optimization of the sieve lets us quit once we reach sqrt(N), and in this case, N = 65,000
-	jb next_prime
-
-
+	push LENGTHOF sieve
+	push OFFSET sieve
+	call sieve_of_Eratosthenes
 
 	mov ecx, LENGTHOF sieve
 	xor edx, edx
